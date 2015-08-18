e14ee2cd65ee629e8552d6c13c8a8f3cc88b6239
Popa Bogdan <bogdy2p@yahoo.com>
2015-08-18 10:07:40 +0300

    Alot of updates & reworks. Implementing custom views from within the code

--- sites/all/modules/custom/siteshortname/classes/MyRestRemoteSelectQuery.class.php
+++ sites/all/modules/custom/siteshortname/classes/MyRestRemoteSelectQuery.class.php
@@ -11,6 +11,297 @@
  *
  * @author pbc
  */
-class MyRestRemoteSelectQuery {
-  //put your code here
+
+/**
+ * Select query for our remote data.
+ *
+ * @todo Make vars protected once no longer developing.
+ */
+class MyRestRemoteSelectQuery extends RemoteEntityQuery {
+
+  /**
+   * Determines whether the query is RetrieveMultiple or Retrieve.
+   *
+   * The query is Multiple by default, until an ID condition causes it to be
+   * single.
+   */
+  public $retrieve_multiple = TRUE;
+
+  /**
+   * An array of conditions on the query. These are grouped by the table they
+   * are on.
+   */
+  public $conditions = array();
+
+  /**
+   * The from date filter for event searches
+   */
+  public $from_date = NULL;
+
+  /**
+   * The to date filter for event searches
+   */
+  public $to_date = NULL;
+
+  /**
+   * The user id.
+   */
+  public $user_id = NULL;
+
+  /**
+   * Constructor to generically set up the user id condition if
+   * there is a current user.
+   *
+   * @param $connection
+   */
+  function __construct($connection) {
+    parent::__construct($connection);
+    if (user_is_logged_in()) {
+      global $user;
+      $this->useridCondition($user->name);
+    }
+  }
+
+  /**
+   * Add a condition to the query.
+   *
+   * Originally based on the entityCondition() method in EntityFieldQuery, but
+   * largely from USDARemoteSelectQuery (Programming Drupal 7 Entities) and
+   * MSDynamicsSoapSelectQuery.
+   *
+   * @param $name
+   *  The name of the entity property.
+   */
+  function entityCondition($name, $value, $operator = NULL) {
+
+    // We only support the entity ID for now.
+    if ($name == 'entity_id') {
+
+      // Get the remote field name of the entity ID.
+      $field = $this->entity_info['remote entity keys']['remote id'];
+
+      // Set the remote ID field to the passed value.
+      $this->conditions[$this->remote_base][] = array(
+        'field' => $field,
+        'value' => $value,
+        'operator' => $operator,
+      );
+
+      // Record that we'll only be retrieving a single item.
+      if (is_null($operator) || ($operator == '=')) {
+        $this->retrieve_multiple = FALSE;
+      }
+    }
+    else {
+
+      // Report an invalid entity condition.
+      $this->throwException(
+          'OURRESTREMOTESELECTQUERY_INVALID_ENTITY_CONDITION', 'The query object can only accept the \'entity_id\' condition.'
+      );
+    }
+  }
+
+  /**
+   * Add a condition to the query, using local property keys.
+   *
+   * Based on MSDynamicsSoapSelectQuery::propertyCondition().
+   *
+   * @param $property_name
+   *  A local property. Ie, a key in the $entity_info 'property map' array.
+   */
+  function propertyCondition($property_name, $value, $operator = NULL) {
+
+    // Make sure the entity base has been set up.
+    if (!isset($this->entity_info)) {
+      $this->throwException(
+          'OURRESTREMOTESELECTQUERY_ENTITY_BASE_NOT_SET', 'The query object was not set with an entity type.'
+      );
+    }
+
+    // Make sure that the provided property is valid.
+    if (!isset($this->entity_info['property map'][$property_name])) {
+      $this->throwException(
+          'OURRESTREMOTESELECTQUERY_INVALID_PROPERY', 'The query object cannot set a non-existent property.'
+      );
+    }
+
+    // Adding a field condition (probably) automatically makes this a multiple.
+    // TODO: figure this out for sure!
+    $this->retrieve_multiple = TRUE;
+
+    // Use the property map to determine the remote field name.
+    $remote_field_name = $this->entity_info['property map'][$property_name];
+
+    // Set the condition for use during execution.
+    $this->conditions[$this->remote_base][] = array(
+      'field' => $remote_field_name,
+      'value' => $value,
+      'operator' => $operator,
+    );
+  }
+
+  /**
+   * Add a user id condition to the query.
+   *
+   * @param $user_id
+   *   The user to search for appointments.
+   */
+  function useridCondition($user_id) {
+    $this->user_id = $user_id;
+  }
+
+  /**
+   * Run the query and return a result.
+   *
+   * @return
+   *  Remote entity objects as retrieved from the remote connection.
+   */
+  function execute() {
+
+    // If there are any validation errors, don't perform a search.
+    if (form_set_error()) {
+      return array();
+    }
+
+    $querystring = array();
+
+    $path = variable_get($this->base_entity_type . '_resource_name', '');
+
+    // Iterate through all of the conditions and add them to the query.
+    if (isset($this->conditions[$this->remote_base])) {
+      foreach ($this->conditions[$this->remote_base] as $condition) {
+        switch ($condition['field']) {
+          case 'event_id':
+            $querystring['eventId'] = $condition['value'];
+            break;
+          case 'login_id':
+            $querystring['userId'] = $condition['value'];
+            break;
+        }
+      }
+    }
+
+    // "From date" parameter.
+    if (isset($this->from_date)) {
+      $querystring['startDate'] = $this->from_date;
+    }
+
+    // "To date" parameter.
+    if (isset($this->to_date)) {
+      $querystring['endDate'] = $this->to_date;
+    }
+
+    // Add user id based filter if present.
+    if (isset($this->user_id)) {
+      $querystring['userId'] = $this->user_id;
+    }
+
+    // Assemble all of the query parameters.
+    if (count($querystring)) {
+      $path .= '?' . drupal_http_build_query($querystring);
+    }
+
+    // Make the request.
+    try {
+      $response = $this->connection->makeRequest($path, 'GET');
+    }
+    catch (Exception $e) {
+      if ($e->getCode() == OUR_REST_LOGIN_REQUIRED_NO_SESSION) {
+        drupal_set_message($e->getMessage());
+        drupal_goto('user/login', array('query' => drupal_get_destination()));
+      }
+      elseif ($e->getCode() == OUR_REST_LOGIN_REQUIRED_TOKEN_EXPIRED) {
+
+        // Logout
+        global $user;
+        module_invoke_all('user_logout', $user);
+        session_destroy();
+
+        // Redirect
+        drupal_set_message($e->getMessage());
+        drupal_goto('user/login', array('query' => drupal_get_destination()));
+      }
+    }
+
+    switch ($this->base_entity_type) {
+      case 'siteshortname_entities_remote_event' :
+        $entities = $this->parseEventResponse($response);
+        break;
+    }
+
+    // Return the list of results.
+    return $entities;
+  }
+
+  /**
+   * Helper for execute() which parses the JSON response for event entities.
+   *
+   * May also set the $total_record_count property on the query, if applicable.
+   *
+   * @param $response
+   *  The JSON/XML/whatever response from the REST server.
+   *
+   * @return
+   *  An list of entity objects, keyed numerically.
+   *  An empty array is returned if the response contains no entities.
+   *
+   * @throws
+   *  Exception if a fault is received when the REST call was made.
+   */
+  function parseEventResponse($response) {
+
+    // Fetch the list of events.
+    if ($response->code == 404) {
+      // No data was returned so let's provide an empty list.
+      $events = array();
+    }
+    else /* we have response data */ {
+
+      // Convert the JSON (assuming that's what we're getting) into a PHP array.
+      // Do any unmarshalling to convert the response data into a PHP array.
+      $events = json_decode($response->data, TRUE);
+    }
+
+    // Initialize an empty list of entities for returning.
+    $entities = array();
+
+    // Iterate through each event.
+    foreach ($events as $event) {
+      $entities[] = (object) array(
+            // Set event information.
+            'event_id' => isset($event['id']) ? $event['id'] : NULL,
+            'event_name' => isset($event['name']) ? $event['name'] : NULL,
+            'event_date' => isset($event['date']) ? $event['date'] : NULL,
+      );
+    }
+
+    // Return the newly-created list of entities.
+    return $entities;
+  }
+
+  /**
+   * Throw an exception when there's a problem.
+   *
+   * @param string $code
+   *   The error code.
+   *
+   * @param string $message
+   *   A user-friendly message describing the problem.
+   *
+   * @throws Exception
+   */
+  function throwException($code, $message) {
+
+    // Report error to the logs.
+    watchdog('siteshortname_entities_remote', 'ERROR: OurRestRemoteSelectQuery: "@code", "@message".', array(
+      '@code' => $code,
+      '@message' => $message,
+    ));
+
+    // Throw an error with which callers must deal.
+    throw new Exception(t("OurRestRemoteSelectQuery error, got message '@message'.", array(
+      '@message' => $message,
+    )), $code);
+  }
+
 }
--- sites/all/modules/custom/siteshortname/classes/clients_connection_my_rest.class.php
+++ sites/all/modules/custom/siteshortname/classes/clients_connection_my_rest.class.php
@@ -7,10 +7,226 @@
  */
 
 /**
+ * @file
  * Description of clients_connection_my_rest
  *
  * @author pbc
  */
-class clients_connection_my_rest {
-  //put your code here
+
+/**
+ * Set up a client connection to our REST services.
+ *
+ *  @todo Make private functions private once development is done.
+ */
+class clients_connection_my_rest extends clients_connection_base implements ClientsConnectionAdminUIInterface, ClientsRemoteEntityInterface {
+  /*   * ************************************************************************
+   * ClientsRemoteEntityInterface implementations.
+   * ************************************************************************ */
+
+  /**
+   * Load a remote entity.
+   *
+   * @param $entity_type
+   *   The entity type to load.
+   * @param $id
+   *   The (remote) ID of the entity.
+   *
+   * @return
+   *  An entity object.
+   */
+  function remote_entity_load($entity_type, $id) {
+    $query = $this->getRemoteEntityQuery('select');
+    $query->base($entity_type);
+    $query->entityCondition('entity_id', $id);
+    $result = $query->execute();
+
+    // There's only one. Same pattern as entity_load_single().
+    return reset($result);
+  }
+
+  /**
+   * Provide a map of remote property types to Drupal types.
+   *
+   * Roughly analogous to _entity_metadata_convert_schema_type().
+   *
+   * @return
+   *   An array whose keys are remote property types as used as types for fields
+   *   in hook_remote_entity_query_table_info(), and whose values are types
+   *   recognized by the Entity Metadata API (as listed in the documentation for
+   *   hook_entity_property_info()).
+   *   If a remote property type is not listed here, it will be mapped to 'text'
+   *   by default.
+   */
+  function entity_property_type_map() {
+    return array(
+      'EntityCollection' => 'list<string>',
+    );
+  }
+
+  /**
+   * Get a new RemoteEntityQuery object appropriate for the connection.
+   *
+   * @param $query_type
+   *  (optional) The type of the query. Defaults to 'select'.
+   *
+   * @return
+   *  A remote query object of the type appropriate to the query type.
+   */
+  function getRemoteEntityQuery($query_type = 'select') {
+    switch ($query_type) {
+      case 'select':
+        return new OurRestRemoteSelectQuery($this);
+      case 'insert':
+        return new OurRestRemoteInsertQuery($this);
+      case 'update':
+        return new OurRestRemoteUpdateQuery($this);
+    }
+  }
+
+  public function remote_entity_save($entity_type, $entity, $remote_properties = array()) {
+    
+  }
+
+  /*   * ************************************************************************
+   * clients_connection_base overrides
+   * ************************************************************************ */
+
+  /**
+   * Call a remote method with an array of parameters.
+   *
+   * This is intended for internal use from callMethod() and
+   * clients_connection_call().
+   * If you need to call a method on given connection object, use callMethod
+   * which has a nicer form.
+   *
+   * Subclasses do not necessarily have to override this method if their
+   * connection type does not make sense with this.
+   *
+   * @param $method
+   *  The name of the remote method to call.
+   * @param $method_params
+   *  An array of parameters to passed to the remote method.
+   *
+   * @return
+   *  Whatever is returned from the remote site.
+   *
+   * @throws Exception on error from the remote site.
+   *  It's up to subclasses to implement this, as the test for an error and
+   *  the way to get information about it varies according to service type.
+   */
+  function callMethodArray($method, $method_params = array()) {
+
+    switch ($method) {
+      case 'makeRequest':
+
+        // Set the parameters.
+        $resource_path = $method_params[0];
+        $http_method = $method_params[1];
+        $data = isset($method_params[2]) ? $method_params[2] : array();
+
+        // Make the request.
+        $results = $this->makeRequest($resource_path, $http_method, $data);
+        break;
+    }
+
+    return $results;
+  }
+
+  /*   * ************************************************************************
+   * Local methods
+   * ************************************************************************ */
+
+  /**
+   * Make a REST request.
+   *
+   * Originally from clients_connection_drupal_services_rest_7->makeRequest().
+   * Examples:
+   * Retrieve an event:
+   *  makeRequest('event?eventId=ID', 'GET');
+   * Update a node:
+   *  makeRequest('node/NID', 'POST', $data);
+   *
+   * @param $resource_path
+   *  The path of the resource. Eg, 'node', 'node/1', etc.
+   * @param $http_method
+   *  The HTTP method. One of 'GET', 'POST', 'PUT', 'DELETE'. For an explanation
+   *  of how the HTTP method affects the resource request, see the Services
+   *  documentation at http://drupal.org/node/783254.
+   * @param $data = array()
+   *  (Optional) An array of data to pass to the request.
+   * @param boolean $data_as_headers
+   *   Data will be sent in the headers if this is set to TRUE.
+   *
+   * @return
+   *  The data from the request response.
+   *
+   *  @todo Update the first two test classes to not assume a SimpleXMLElement.
+   */
+  function makeRequest($resource_path, $http_method, $data = array(), $data_as_headers = FALSE) {
+
+    // Tap into this function's cache if there is one.
+    $request_cache_map = &drupal_static(__FUNCTION__);
+
+    // Set the options.
+    $options = array(
+      'headers' => $this->getHeaders(), // Define if you need it.
+      'method' => $http_method,
+      'data' => $data,
+    );
+
+    // If cached, we have already issued this request during this page request so
+    // just use the cached value.
+    $request_path = $this->endpoint . $context_path . '/' . $resource_path;
+
+    // Either get the data from the cache or send a request for it.
+    if (isset($request_cache_map[$request_path])) {
+      // Use the cached copy.
+      $response = $request_cache_map[$request_path];
+    }
+    else {
+      // Not cached yet so fire off the request.
+      $response = drupal_http_request($request_path, $options);
+
+      // And then cache to avoid duplicate calls within the page request.
+      $request_cache_map[$request_path] = $response;
+    }
+
+    // Handle any errors and then return the response.
+    $this->handleRestError($request_path, $response);
+    return $response;
+  }
+
+  /**
+   * Common helper for reacting to an error from a REST call.
+   *
+   * Originally from clients_connection_drupal_services_rest_7->handleRestError().
+   * Gets the error from the response, logs the error message,
+   * and throws an exception, which should be caught by the module making use
+   * of the Clients connection API.
+   *
+   * @param $response
+   *  The REST response data, decoded.
+   *
+   * @throws Exception
+   */
+  function handleRestError($request, $response) {
+
+    // Report and throw an error if we get anything unexpected.
+    if (!in_array($response->code, array(200, 201, 202, 204, 404))) {
+
+      // Report error to the logs.
+      watchdog('clients', 'Error with REST request (@req). Error was code @code with error "@error" and message "@message".', array(
+        '@req' => $request,
+        '@code' => $response->code,
+        '@error' => $response->error,
+        '@message' => isset($response->status_message) ? $response->status_message : '(no message)',
+          ), WATCHDOG_ERROR);
+
+      // Throw an error with which callers must deal.
+      throw new Exception(t("Clients connection error, got message '@message'.", array(
+        '@message' => isset($response->status_message) ? $response->status_message : $response->error,
+      )), $response->code);
+    }
+  }
+
 }
