e6ba8f98efb0d7ded29925f2c117e063588c4f75
Popa <bogdy2p@yahoo.com>
2015-08-24 14:54:33 +0300

    Some cleanup @ character3D

--- sites/all/modules/custom/wowitems/character3D.php
+++ sites/all/modules/custom/wowitems/character3D.php
@@ -14,6 +14,8 @@
 class character3D {
 
 //put your code here
+  public $error_messages;
+  public $errors = 0;
   public $characterName;
   public $guid;
   public $race;
@@ -53,20 +55,27 @@ class character3D {
 
   public function initialize($characterName) {
     $this->get_the_character($characterName);
-    $this->assign_character_variables($this->character_data);
-    $this->set_character_features($this->character_data);
+    $this->assign_character_variables();
+    $this->set_character_features();
+    $this->get_character_items();
+    $this->get_item_entry();
+    $this->get_display_ids_and_inventory_types();
+    $this->create_the_eq_string();
+    $this->create_the_div_and_display_the_character();
   }
 
   /**
    * Assign/set character variables
    * @param type $character_data
    */
-  function assign_character_variables($character_data) {
-    $this->guid = $character_data['guid'];
-    $this->race = $character_data['race'];
-    $this->gender = $character_data['gender'];
-    $this->b = $character_data['playerBytes'];
-    $this->b2 = $character_data['playerBytes2'];
+  function assign_character_variables() {
+    if ($this->errors == 0) {
+      $this->guid = $this->character_data['guid'];
+      $this->race = $this->character_data['race'];
+      $this->gender = $this->character_data['gender'];
+      $this->b = $this->character_data['playerBytes'];
+      $this->b2 = $this->character_data['playerBytes2'];
+    }
   }
 
   /**
@@ -74,14 +83,15 @@ class character3D {
    * Set Character Race/Gender
    * @param type $character_data
    */
-  public function set_character_features($character_data) {
-
-    $this->ha = ($this->b >> 16) % 256;
-    $this->hc = ($this->b >> 24) % 256;
-    $this->fa = ($this->b >> 8) % 256;
-    $this->sk = $this->b % 256;
-    $this->fh = $this->b2 % 256;
-    $this->rg = $this->char_race[$this->race] . $this->char_gender[$this->gender];
+  public function set_character_features() {
+    if ($this->errors == 0) {
+      $this->ha = ($this->b >> 16) % 256;
+      $this->hc = ($this->b >> 24) % 256;
+      $this->fa = ($this->b >> 8) % 256;
+      $this->sk = $this->b % 256;
+      $this->fh = $this->b2 % 256;
+      $this->rg = $this->char_race[$this->race] . $this->char_gender[$this->gender];
+    }
   }
 
   /*
@@ -90,103 +100,123 @@ class character3D {
    */
 
   function get_the_character($charname) {
-    db_set_active('characters');
-    $query_select_character = "SELECT guid, race, gender, playerBytes, playerBytes2 FROM characters WHERE name = '$charname' LIMIT 1";
-    $result_select_character = db_query($query_select_character);
-    db_set_active('default');
-    if ($result_select_character->rowCount() == 1) {
-      $this->character_data = $result_select_character->fetchAssoc();
-      return $this;
-    }
-    else {
-      return NULL;
+    if ($this->errors == 0) {
+      db_set_active('characters');
+      $query_select_character = "SELECT guid, race, gender, playerBytes, playerBytes2 FROM characters WHERE name = '$charname' LIMIT 1";
+      $result_select_character = db_query($query_select_character);
+      db_set_active('default');
+      if ($result_select_character->rowCount() == 1) {
+        $this->character_data = $result_select_character->fetchAssoc();
+        return $this;
+      }
+      else {
+        $this->error_messages[] = "Could not retrieve the character using the specified name. Error.";
+        $this->errors++;
+        return NULL;
+      }
     }
   }
 
   /**
    * Get the character's currently equipped items 
-   * @return array of Item Objects
+   * @return sets the items_array property of the object.
    */
   function get_character_items() {
-    $items = array();
-    db_set_active('characters');
-    $guid = $this->character_data['guid'];
-    $query_select_guid = "SELECT item FROM character_inventory WHERE guid = '$guid' AND bag='0' AND slot <'18'";
-    $result_select_guid = db_query($query_select_guid);
-    db_set_active('default');
-
-    $items = $result_select_guid->fetchAll();
-    if ($items) {
-      $this->items_array = $items;
-      return;
+    if ($this->errors == 0) {
+      $items = array();
+      db_set_active('characters');
+      $guid = $this->character_data['guid'];
+      $query_select_guid = "SELECT item FROM character_inventory WHERE guid = '$guid' AND bag='0' AND slot <'18'";
+      $result_select_guid = db_query($query_select_guid);
+      db_set_active('default');
+      $items = $result_select_guid->fetchAll();
+      if ($items) {
+        $this->items_array = $items;
+        return;
+      }
+      $this->error_messages[] = "Could not retrieve the character's items. Error.";
+      $this->errors++;
+      return null;
     }
-    return null;
   }
 
   /**
-   * 
+   * Return the item_entry for the current item.
    */
   function get_item_entry() {
-    $items_array = $this->items_array;
-    db_set_active('characters');
-    foreach ($items_array as $item_in_array) {
-      $itemString = $item_in_array->item;
-//      print_r($itemString);
-      $query_select_itemEntry = "SELECT itemEntry FROM item_instance WHERE guid = '$itemString'";
-      $result_select_itemEntry = db_query($query_select_itemEntry);
-      $this->item_entries[$itemString] = $result_select_itemEntry->fetch();
-    }
-    db_set_active('default');
-  }
-
-  /**
-   * 
-   */
-  function get_display_ids_and_inventory_types() {
-    if ($this->item_entries != "") {
-      $items_array = $this->item_entries;
-
-      db_set_active('world');
-      foreach ($this->item_entries as $itemEntry) {
-        $query_select_item_drupal = "SELECT displayid, InventoryType FROM item_template WHERE entry = '$itemEntry->itemEntry' LIMIT 1";
-        $result_select_item_drupal = db_query($query_select_item_drupal);
-        $this->displayid_inventoryType[] = $result_select_item_drupal->fetch();
+    if ($this->errors == 0) {
+      $items_array = $this->items_array;
+      db_set_active('characters');
+      foreach ($items_array as $item_in_array) {
+        $itemString = $item_in_array->item;
+        $query_select_itemEntry = "SELECT itemEntry FROM item_instance WHERE guid = '$itemString'";
+        $result_select_itemEntry = db_query($query_select_itemEntry);
+        $this->item_entries[$itemString] = $result_select_itemEntry->fetch();
       }
       db_set_active('default');
     }
   }
-/**
- * 
- */
-  function test() {
-    foreach ($this->displayid_inventoryType as $row_item) {
-      $displayid = $row_item->displayid;
-      $inventory_type = $row_item->InventoryType;
-      if ($this->eq == "") {
-        $this->eq = $inventory_type . ',' . $displayid;
-      }
-      else {
-       $this->eq .= ',' . $inventory_type . ',' . $displayid;
+
+  /**
+   * Return the display id and inventory type for the current item.
+   */
+  function get_display_ids_and_inventory_types() {
+    if ($this->errors == 0) {
+      if ($this->item_entries != "") {
+        $items_array = $this->item_entries;
+
+        db_set_active('world');
+        foreach ($this->item_entries as $itemEntry) {
+          $query_select_item_drupal = "SELECT displayid, InventoryType FROM item_template WHERE entry = '$itemEntry->itemEntry' LIMIT 1";
+          $result_select_item_drupal = db_query($query_select_item_drupal);
+          $this->displayid_inventoryType[] = $result_select_item_drupal->fetch();
+        }
+        db_set_active('default');
       }
     }
-//    print_r($this->eq);
   }
+
+  /**
+   * Create the string to be sent to the model viewer
+   */
+  function create_the_eq_string() {
+    if ($this->errors == 0) {
+      foreach ($this->displayid_inventoryType as $row_item) {
+        $displayid = $row_item->displayid;
+        $inventory_type = $row_item->InventoryType;
+        if ($this->eq == "") {
+          $this->eq = $inventory_type . ',' . $displayid;
+        }
+        else {
+          $this->eq .= ',' . $inventory_type . ',' . $displayid;
+        }
+      }
+    }
+  }
+
   /**
    * 
    */
-  function complete(){
-//     if ($errors == 0) {
-    echo '<div id="model_scene" align="center">';
-    echo '<object id="wowhead" type="application/x-shockwave-flash" data="http://static.wowhead.com/modelviewer/ModelView.swf" height="400px" width="300px">';
-    echo '<param name="quality" value="high">';
-    echo '<param name="allowscriptaccess" value="always">';
-    echo '<param name="menu" value="false">';
-    echo '<param value="transparent" name="wmode">';
-    echo printf('<param name="flashvars" value="model=%s&amp;modelType=16&amp;ha=%s&amp;hc=%s&amp;fa=%s&amp;sk=%s&amp;fh=%s&amp;fc=0&amp;contentPath=http://static.wowhead.com/modelviewer/&amp;blur=0&amp;equipList=%s">', $this->rg, $this->ha, $this->hc, $this->fa, $this->sk, $this->fh, $this->eq);
-    echo '<param name="movie" value="http://static.wowhead.com/modelviewer/ModelView.swf">';
-    echo '</object>';
-    echo '</div>';
+  function create_the_div_and_display_the_character() {
+    if ($this->errors == 0) {
+      echo '<div id="model_scene" align="center">';
+      echo '<object id="wowhead" type="application/x-shockwave-flash" data="http://static.wowhead.com/modelviewer/ModelView.swf" height="400px" width="300px">';
+      echo '<param name="quality" value="high">';
+      echo '<param name="allowscriptaccess" value="always">';
+      echo '<param name="menu" value="false">';
+      echo '<param value="transparent" name="wmode">';
+      echo printf('<param name="flashvars" value="model=%s&amp;modelType=16&amp;ha=%s&amp;hc=%s&amp;fa=%s&amp;sk=%s&amp;fh=%s&amp;fc=0&amp;contentPath=http://static.wowhead.com/modelviewer/&amp;blur=0&amp;equipList=%s">', $this->rg, $this->ha, $this->hc, $this->fa, $this->sk, $this->fh, $this->eq);
+      echo '<param name="movie" value="http://static.wowhead.com/modelviewer/ModelView.swf">';
+      echo '</object>';
+      echo '</div>';
+    }
+    else {
+      print_r("There were " . $this->errors . " errors. ");
+      foreach ($this->error_messages as $error_message) {
+        print_r("<br />");
+        print_r($error_message);
+      }
+    }
   }
-//}
 
 }
